8b997923a4956937324478584b3d553a7145b475
babak <babak@peachscore.com>
2023-08-11 15:31:16 +0300

    fix: remove pupeeteer

--- src/adapter/out/persistence/reportRepository.py
+++ src/adapter/out/persistence/reportRepository.py
@@ -1,26 +1,24 @@
 import jinja2
-import subprocess
+from html2image import Html2Image
 class ReportRepository:
+
+    def __init__(self):
+        self.hti = Html2Image(output_path="src/public/images")
 
     def create_report(self, report, template_name):
         template_loader = jinja2.FileSystemLoader(searchpath="src/templates")
         template_env = jinja2.Environment(loader=template_loader)
         template = template_env.get_template(template_name)
         output = template.render(report)
-        html_file_path = "src/public/html/report.html"
-        with open(html_file_path, "w") as file:
-            file.write(output)
 
-        return self.save_to_image(html_file_path, "src/public/images/1.png")
 
-    def save_to_image(self, html_file_path: str, image_file_path: str):
+        return self.save_to_image(output, f"{report['campaign_id']}.png")
+
+    def save_to_image(self, output, image_name: str):
         try:
-            result = subprocess.run(['node', 'src/puputeer_app/html_to_image.js', html_file_path, image_file_path], capture_output=True, text=True)
-            if result.returncode != 0:
-                print("An error occurred:", result.stderr)
-            else:
-                print("image successfully created", result.stdout, result.stderr)
-            return image_file_path
+            self.hti.screenshot(html_str=output, save_as=image_name, )
+    
+            return f"src/public/images/{image_name}"
 
 
         except Exception as e:
