2fb26c18c7627732897d438c928175ef02c1c421
babak <babak@peachscore.com>
2023-08-11 15:49:39 +0300

    fix: flags

--- src/adapter/out/persistence/reportRepository.py
+++ src/adapter/out/persistence/reportRepository.py
@@ -4,7 +4,7 @@ path_to_chrome = "/usr/bin/google-chrome-stable"
 class ReportRepository:
 
     def __init__(self):
-        self.hti = Html2Image(output_path="src/public/images", browser_executable=path_to_chrome, custom_flags=['--disable-gpu', '--no-sandbox'])
+        self.hti = Html2Image(output_path="src/public/images", browser_executable=path_to_chrome, custom_flags=['--disable-gpu', '--no-sandbox', '--disable-software-rasterizer'])
 
     def create_report(self, report, template_name):
         template_loader = jinja2.FileSystemLoader(searchpath="src/templates")
